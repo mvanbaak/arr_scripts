51a61a62c95ed2e8533a907d6e68808d348b2652
Michiel van Baak Jansen <michiel@vanbaak.eu>
2026-06-30 04:18:32 +0200

    fix: use jq --arg to safely interpolate tag labels

--- radarr/connect/tag_dvfelmel.sh
+++ radarr/connect/tag_dvfelmel.sh
@@ -72,7 +72,7 @@ get_tag_id_by_label() {
         -H "Accept-Encoding: application/json" \
         -H "X-Api-Key: ${RADARR_API_KEY}" \
         "${RADARR_API_URL}/tag" | \
-    jq ".[] | select(.label == \"$1\") | .id"
+    jq --arg t "$1" '.[] | select(.label == $t) | .id'
 }
 
 create_tag() {
